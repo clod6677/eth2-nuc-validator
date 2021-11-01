9959f9cb256b70a1190da3f3c5709ad7e33a2dbc
Claudio Vanoni <vanoniclaudio@gmail.com>
2021-11-01 12:27:05 +0100

    add prune script

---
+++ scripts/start-geth-prune.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Starts a local fast-synced geth node.
+
+DEFAULT_NETWORK=mainnet
+
+if [ "$NETWORK" = "" ]; then
+	NETWORK=$DEFAULT_NETWORK
+fi
+
+
+if [ "$START_GETH" != "" ]; then
+	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
+		exec geth snapshot prune-state --goerli
+	else
+		exec geth snapshot prune-state  --datadir /root/.ethereum/geth
+	fi
+fi
