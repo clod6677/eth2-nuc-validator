8fcba517f99d78ec4c46debe4c97a23ec4fd4e03
Claudio Vanoni <vanoniclaudio@gmail.com>
2022-09-01 03:10:37 +0200

    expose metrics from geth

--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -11,9 +11,9 @@ fi
 
 if [ "$START_GETH" != "" ]; then
 	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
-                exec geth --authrpc.addr "0.0.0.0" --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/scripts/jwtsecret
+                exec geth --authrpc.addr "0.0.0.0" --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/scripts/jwtsecret --metrics --metrics.addr="0.0.0.0"
 	else
-		exec geth --authrpc.addr "0.0.0.0" --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/scripts/jwtsecret
+		exec geth --authrpc.addr "0.0.0.0" --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/scripts/jwtsecret --metrics --metrics.addr="0.0.0.0"
 		#exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable --metrics --metrics.addr="0.0.0.0"
 	fi
 fi
